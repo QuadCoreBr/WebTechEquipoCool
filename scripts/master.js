aea80c9d27eb704d97e6d3f36d7a5f9d28bbfcda
gisele <giselee.mau@gmail.com>
2017-03-30 16:05:16 -0600

    Web :3

--- scripts/master.js
+++
@@ -1,8 +0,0 @@
-function burguerClic() {
-    var x = document.getElementById("topNav");
-    if (x.className === "menu-bar") {
-        x.className += " burguer";
-    } else {
-        x.className = "menu-bar";
-    }
-}
